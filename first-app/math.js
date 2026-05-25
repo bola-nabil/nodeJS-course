37097b33d5fca3eb379f81e07415eea7044b65f0
Bola Nabil <bolanabil404@gmail.com>
2026-05-25 19:25:34 +0300

    export fn add and sub with module.exports

---
+++ first-app/math.js
@@ -0,0 +1,19 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// module.exports = add;
+
+// module.exports = function(a,b) {
+//     return a + b;
+// }
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+module.exports = {add, sub}; 
