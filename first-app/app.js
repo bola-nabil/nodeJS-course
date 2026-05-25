cbd3adb42d484db35ee40c2afb390a402665cd54
Bola Nabil <bolanabil404@gmail.com>
2026-05-25 19:32:45 +0300

    import fn mul and divide using import moudle

--- first-app/app.js
+++ first-app/app.js
@@ -3,7 +3,12 @@
 // console.log(math.add(1,2));
 // console.log(math.sub(5,2));
 
-const {add, sub} = require("./math.js");
+// const {add, sub} = require("./math.js");
 
-console.log(add(1, 2));
-console.log(sub(5,2));
+// console.log(add(1, 2));
+// console.log(sub(5,2));
+
+import {divide, mul} from "./calc.js";
+
+console.log(mul(2, 5));
+console.log(divide(6,2));
