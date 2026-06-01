5f8ae87f52da354dfad7902fa0160cf3d86e1267
Bola Nabil <bolanabil404@gmail.com>
2026-06-01 14:00:14 +0300

    test divide and mul with module type

--- first-app/app.js
+++ first-app/app.js
@@ -1,14 +1,12 @@
 // const math = require("./math.js");
-
 // console.log(math.add(1,2));
 // console.log(math.sub(5,2));
-
 // const {add, sub} = require("./math.js");
-
 // console.log(add(1, 2));
 // console.log(sub(5,2));
 
 import {divide, mul} from "./calc.js";
+
 
 console.log(mul(2, 5));
 console.log(divide(6,2));
