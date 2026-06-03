016656519bf3bb0d9e4b5cc31a20a570d24eda8e
Bola Nabil <bolanabil404@gmail.com>
2026-06-03 15:26:02 +0300

    writeFileSync

--- first-app/fsModule.js
+++ first-app/fsModule.js
@@ -1,4 +1,7 @@
-import {readFileSync} from "fs";
+import {readFileSync, writeFileSync} from "fs";
 
 const testingContent = readFileSync("./content/test.txt", "utf-8");
-console.log(testingContent);
+
+// writeFileSync
+writeFileSync("./content/result.txt", "Result of test"); // create result file
+const readResultTesting = readFileSync("./content/result.txt", "utf-8");
