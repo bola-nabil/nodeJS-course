649197eef862e430a042afd96aa2719d30c6cb29
Bola Nabil <bolanabil404@gmail.com>
2026-06-03 15:35:48 +0300

    readFile

--- first-app/fsModule.js
+++ first-app/fsModule.js
@@ -1,7 +1,18 @@
 import {readFileSync, writeFileSync} from "fs";
+import {readFile} from "fs/promises";
 
-const testingContent = readFileSync("./content/test.txt", "utf-8");
+// const testingContent = readFileSync("./content/test.txt", "utf-8");
 
 // writeFileSync
-writeFileSync("./content/result.txt", "Result of test"); // create result file
-const readResultTesting = readFileSync("./content/result.txt", "utf-8");
+// writeFileSync("./content/result.txt", "Result of test"); // create result file
+// const readResultTesting = readFileSync("./content/result.txt", "utf-8");
+
+// readFile
+// readFile("./content/test.txt", "utf-8", (err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+// readFile Modern version
+const data = await readFile("./content/result.txt", "utf-8");
+console.log(data);
