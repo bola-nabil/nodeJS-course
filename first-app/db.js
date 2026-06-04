4924a310a8b55413844fe9395e008eb5be66d3c9
Bola Nabil <bolanabil404@gmail.com>
2026-06-04 19:22:13 +0300

    Task 4 of fs Module

---
+++ first-app/db.js
@@ -0,0 +1,23 @@
+import {readFile, writeFile} from "fs/promises";
+
+async function addUser(name) {
+
+    let usersData = [];
+    const users = await readFile("./database.json", "utf-8");
+
+    usersData = JSON.parse(users);
+    const lastId = usersData.length;
+
+    const newUser = {
+        id: lastId + 1,
+        name: name
+    };
+
+
+    usersData.push(newUser)
+
+
+    await writeFile("./database.json", `${JSON.stringify(usersData)}`);
+}
+
+addUser("bola");
