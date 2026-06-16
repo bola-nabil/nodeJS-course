72237804ecee39310cc46e83dc3f1f959e837c53
Bola Nabil <bolanabil404@gmail.com>
2026-06-16 13:21:15 +0300

    Writable Stream example

--- first-app/streamsModule.js
+++ first-app/streamsModule.js
@@ -1,8 +1,14 @@
-import {createReadStream} from "fs";
+import {createReadStream, createWriteStream} from "fs";
 
 // Readable Stream
 const readStream = createReadStream("./content/index.txt", "utf-8");
 
 readStream.on("data", (chunk) => {
     console.log("Chunk:", chunk);
-})
+});
+
+// Writable Stream
+const writeStream = createWriteStream("./content/ouput.txt");
+writeStream.write("Hello ");
+writeStream.write("World !");
+writeStream.end();
