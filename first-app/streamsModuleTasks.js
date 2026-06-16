4f39c838ec3077885f922e77ea947c2948f7a84f
Bola Nabil <bolanabil404@gmail.com>
2026-06-16 14:17:11 +0300

    task2: Create a writable stream and write your name 5 times

--- first-app/streamsModuleTasks.js
+++ first-app/streamsModuleTasks.js
@@ -1,4 +1,4 @@
-import {createReadStream} from "fs";
+import {createReadStream, createWriteStream} from "fs";
 
 // task 1: Create a readable stream and print file content chunk by chunk
 const readStream = createReadStream("./content/index.txt", "utf-8");
@@ -12,5 +12,20 @@ readStream.on("end", () => {
 });
 
 readStream.on("error", (err) => {
+    console.log("Stream Error", err.message);
+});
+
+// task2: Create a writable stream and write your name 5 times
+const writeStream = createWriteStream("./content/ouput.txt");
+for(let i=0; i<5; i++) {
+    writeStream.write("bola ");
+}
+writeStream.end();
+
+writeStream.on("finish", () => {
+    console.log("Writing finished!");
+});
+
+writeStream.on("error", (err) => {
     console.log("Stream Error", err.message);
 });
