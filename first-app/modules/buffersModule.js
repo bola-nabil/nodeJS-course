2def9a3627a333d65e77938cd7d70e66a73aa300
Bola Nabil <bolanabil404@gmail.com>
2026-06-21 18:54:38 +0300

    Create a buffer from a string

--- first-app/modules/buffersModule.js
+++ first-app/modules/buffersModule.js
@@ -1,2 +1,5 @@
 // Creates an empty buffer filled with zeros
 const buffer = Buffer.alloc(4); // <Buffer 00 00 00 00>
+
+// Create a buffer from a string
+const bufferFromString = Buffer.from("Hello"); // <Buffer 48 65 6c 6c 6f>
