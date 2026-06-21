fb26d6a242aaa664a054ab90f1b272c059e1bef2
Bola Nabil <bolanabil404@gmail.com>
2026-06-21 19:29:13 +0300

    task 2

--- first-app/modules/buffersModuleTasks.js
+++ first-app/modules/buffersModuleTasks.js
@@ -2,3 +2,8 @@
 const bufferTask1 = Buffer.from("NodeJS");
 console.log(bufferTask1);
 console.log(bufferTask1.toString());
+
+// task 2
+const bufferTask2 = Buffer.alloc(8);
+bufferTask2.write("Backend");
+console.log(bufferTask2.toString());
