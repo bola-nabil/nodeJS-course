3b44195b21827908d132e6aae06e5faff75a3ddc
Bola Nabil <bolanabil404@gmail.com>
2026-06-21 18:56:42 +0300

    Converting Buffer to String

--- first-app/modules/buffersModule.js
+++ first-app/modules/buffersModule.js
@@ -3,3 +3,6 @@ const buffer = Buffer.alloc(4); // <Buffer 00 00 00 00>
 
 // Create a buffer from a string
 const bufferFromString = Buffer.from("Hello"); // <Buffer 48 65 6c 6c 6f>
+
+// Converting Buffer to String
+const covertingStringBuffer = bufferFromString.toString(); // Hello
