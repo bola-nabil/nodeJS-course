a358b2d9cdd81a865e19458f760c5fb6735efc99
Bola Nabil <bolanabil404@gmail.com>
2026-06-23 15:36:38 +0300

    Task 3

--- first-app/ProcessObjectTasks.js
+++ first-app/ProcessObjectTasks.js
@@ -4,3 +4,9 @@ console.log(process.ppid);
 console.log(process.version);
 console.log(process.platform);
 console.log(process.arch);
+
+// Task 2
+const currentDir = process.cwd();
+console.log("Current Dir:", currentDir);
+process.chdir("../");
+console.log("Dir After Changed:", process.cwd());
