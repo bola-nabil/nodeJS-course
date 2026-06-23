fdf7fc1221e4e3f514f82fac9e96896cde9f2110
Bola Nabil <bolanabil404@gmail.com>
2026-06-23 15:45:40 +0300

    Task 3

--- first-app/ProcessObjectTasks.js
+++ first-app/ProcessObjectTasks.js
@@ -10,3 +10,8 @@ const currentDir = process.cwd();
 console.log("Current Dir:", currentDir);
 process.chdir("../");
 console.log("Dir After Changed:", process.cwd());
+
+// Task 3
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+console.log(`Sum = ${num1 + num2}`);
