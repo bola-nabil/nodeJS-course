df90605dceb316abf63c88b17fb35767bb145b2f
Bola Nabil <bolanabil404@gmail.com>
2026-06-25 18:38:08 +0300

    Convert JSON string into JavaScript array.

--- notes-cli/services/notesService.js
+++ notes-cli/services/notesService.js
@@ -1,5 +1,7 @@
 import {readFile} from "fs/promises";
 
-export const loadNotes = () => {
+export const loadNotes = async () => {
+    const readContent = await readFile("notes.json", "utf-8");
 
+    const content = JSON.parse(readContent);
 }
