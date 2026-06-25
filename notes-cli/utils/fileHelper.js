ba125f991420686c82170ada369592dee154f2f9
Bola Nabil <bolanabil404@gmail.com>
2026-06-25 20:01:16 +0300

    add commandChecker to check command order

---
+++ notes-cli/utils/fileHelper.js
@@ -0,0 +1,14 @@
+export const commandChecker = (command) => {
+    const firstArg = process.argv[2];
+    const secondArg = process.argv[3];
+
+    if(!firstArg) {
+        return "Please provide a command.";
+    }
+
+    if(firstArg !== command) {
+            return "Unknown command.";
+    }
+}
+
+export const commandOrder = process.argv[2];
