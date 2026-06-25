cdca7ce067b99d6bb7c4c846aba137d9de594ddf
Bola Nabil <bolanabil404@gmail.com>
2026-06-25 19:45:23 +0300

    test add command

--- notes-cli/index.js
+++ notes-cli/index.js
@@ -1,11 +1,5 @@
 import {addCommand} from "./commands/add.js";
 import {loadNotes, saveNotes} from "./services/notesService.js";
 
-const add = addCommand();
+const add = await addCommand();
 console.log(add);
-
-const notesData = await loadNotes();
-console.log(notesData);
-
-const saveNotesData = await saveNotes([{"id": 1}]);
-console.log(saveNotesData);
