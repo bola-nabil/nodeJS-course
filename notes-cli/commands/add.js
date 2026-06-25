087d5849ba84b444e85924a65c09918d310f6140
Bola Nabil <bolanabil404@gmail.com>
2026-06-25 19:45:52 +0300

    save data

--- notes-cli/commands/add.js
+++ notes-cli/commands/add.js
@@ -1,6 +1,6 @@
-import {loadNotes, saveNotes} from "../services/notesService";
+import { loadNotes, saveNotes } from "../services/notesService.js";
 
-export const addCommand = () => {
+export const addCommand = async() => {
     const firstArg = process.argv[2];
     const secondArg = process.argv[3];
 
@@ -14,6 +14,26 @@ export const addCommand = () => {
    if(firstArg !== "add") {
         return "Unknown command.";
    }
-
    const notesData = await loadNotes();
+
+   const newId =
+  notesData.length === 0
+    ? 1
+    : notesData[notesData.length - 1].id + 1;
+
+   const objectData = {
+     id: newId,
+     title: secondArg,
+     createdAt: new Date().toISOString(),
+   }
+
+   const titleData = notesData.map((data) => data.title);
+
+   if(titleData.includes(secondArg)) {
+     return "Note already exists.";
+   }
+
+   notesData.push(objectData);
+   await saveNotes(notesData);
+   return "Note added successfully.";
 }
