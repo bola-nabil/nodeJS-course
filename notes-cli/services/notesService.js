5e6c9fd8ed4b12d4c5807a7e3d4342cc0722982d
Bola Nabil <bolanabil404@gmail.com>
2026-06-25 18:41:16 +0300

    services/notesService.js

--- notes-cli/services/notesService.js
+++ notes-cli/services/notesService.js
@@ -1,7 +1,11 @@
 import {readFile} from "fs/promises";
 
 export const loadNotes = async () => {
-    const readContent = await readFile("notes.json", "utf-8");
-
-    const content = JSON.parse(readContent);
+    try {
+        const readContent = await readFile("notes.json", "utf-8");
+        const content = JSON.parse(readContent);
+        return content;
+    } catch(err) {
+        return [];
+    }
 }
