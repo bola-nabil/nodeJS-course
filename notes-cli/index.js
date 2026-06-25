1e4a62906618761057a25e38c2d93e57ccbaa5e3
Bola Nabil <bolanabil404@gmail.com>
2026-06-25 18:42:32 +0300

    test and check notes Data

--- notes-cli/index.js
+++ notes-cli/index.js
@@ -1,4 +1,8 @@
 import {addCommand} from "./commands/add.js";
+import {loadNotes} from "./services/notesService.js";
 
 const add = addCommand();
 console.log(add);
+
+const notesData = loadNotes();
+console.log(notesData);
