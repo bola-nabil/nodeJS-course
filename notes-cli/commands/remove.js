de6e143ade7010ed61927869c7d91db83ee4b5cd
Bola Nabil <bolanabil404@gmail.com>
2026-06-27 19:46:20 +0300

    filter data

--- notes-cli/commands/remove.js
+++ notes-cli/commands/remove.js
@@ -1,5 +1,19 @@
 import {commandChecker, commandOrder, notesData} from "../utils/fileHelper.js";
 
 export const remove = () => {
+    commandChecker("remove");
 
+    const target = process.argv[3];
+
+    if(commandOrder === "remove") {
+        const filteredNotes = notesData.filter(
+            note => note.title !== target
+        );
+
+        if (filteredNotes.length === notesData.length) {
+            return "Note not found";
+        }
+
+        return "Successfully removed";
+    }
 }
