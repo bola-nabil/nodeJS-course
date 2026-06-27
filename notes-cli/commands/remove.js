4263847a2a20800053924cfdf1a538557e5a49e4
Bola Nabil <bolanabil404@gmail.com>
2026-06-27 20:13:20 +0300

    make code clean

--- notes-cli/commands/remove.js
+++ notes-cli/commands/remove.js
@@ -1,21 +1,18 @@
-import {commandChecker, commandOrder, notesData} from "../utils/fileHelper.js";
+import {notesData} from "../utils/fileHelper.js";
 import {saveNotes} from "../services/notesService.js";
 
 export const remove = async() => {
-    commandChecker("remove");
 
     const target = process.argv[3];
 
-    if(commandOrder === "remove") {
-        const filteredNotes = notesData.filter(
-            note => note.title !== target
-        );
+    const filteredNotes = notesData.filter(
+        note => note.title !== target
+    );
 
-        if (filteredNotes.length === notesData.length) {
-            return "Note not found";
-        }
-
-        await saveNotes(filteredNotes)
-        return "Successfully removed";
+    if (filteredNotes.length === notesData.length) {
+        return "Note not found";
     }
+
+    await saveNotes(filteredNotes)
+    return "Successfully removed";
 }
