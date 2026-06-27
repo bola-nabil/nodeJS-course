c20f8fc360e4198dc8a4dad506a89ea4f29fcbaa
Bola Nabil <bolanabil404@gmail.com>
2026-06-27 20:06:52 +0300

    command input fn() to enter command order easy and check commands

--- notes-cli/utils/fileHelper.js
+++ notes-cli/utils/fileHelper.js
@@ -1,4 +1,9 @@
 import {loadNotes} from "../services/notesService.js";
+import { add } from "../commands/add.js";
+import { list } from "../commands/list.js";
+import { read } from "../commands/read.js";
+import { remove } from "../commands/remove.js";
+import {update} from "../commands/update.js";
 
 export const commandChecker = (command) => {
     const firstArg = process.argv[2];
@@ -16,3 +21,14 @@ export const commandChecker = (command) => {
 export const commandOrder = process.argv[2];
 
 export const notesData = await loadNotes();
+
+export const commandInput = (command) => {
+    switch(command) {
+        case "add": await add(); break;
+        case "list": list(); break;
+        case "read": read(); break;
+        case "remove": await  remove(); break;
+        case "update": await update(); break;
+        default: "Unknown command."; break;
+    }
+}
