18c881d44a887fcbccd8f8d278240798b2de2921
Bola Nabil <bolanabil404@gmail.com>
2026-06-27 19:16:20 +0300

    search result about target

--- notes-cli/commands/read.js
+++ notes-cli/commands/read.js
@@ -1,5 +1,13 @@
 import {commandChecker, commandOrder, notesData} from "../utils/fileHelper.js";
 
 export const read = () => {
+    commandChecker("read");
 
+    const target = process.argv[3];
+
+    const foundSearch = notesData.find((data) => data.title === target);
+
+    if(commandOrder === "read") {
+        return foundSearch;
+    }
 }
