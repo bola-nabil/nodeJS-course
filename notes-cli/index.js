61b94d7a7d81ff25e964618c0b92b8bb6f55f98d
Bola Nabil <bolanabil404@gmail.com>
2026-06-27 19:24:59 +0300

    test remove command

--- notes-cli/index.js
+++ notes-cli/index.js
@@ -1,6 +1,7 @@
 import {add} from "./commands/add.js";
 import {list} from "./commands/list.js";
 import {read} from "./commands/read.js";
+import {remove} from "./commands/remove.js";
 
 const addCommand = await add();
 console.log(addCommand);
@@ -12,3 +13,7 @@ console.log(listCommand);
 // read command
 const readCommand = read();
 console.log(readCommand);
+
+// remove command
+const removeCommand = remove();
+console.log(removeCommand);
