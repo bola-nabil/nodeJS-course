afd17fce4afbca76beefd4c4ea32bc23431daeeb
Bola Nabil <bolanabil404@gmail.com>
2026-06-28 14:16:19 +0300

    handle if notesDate is empty

--- notes-cli/commands/list.js
+++ notes-cli/commands/list.js
@@ -3,6 +3,10 @@ import { loadNotes } from "../services/notesService";
 export const list = () => {
       const notesData = await loadNotes();
 
+      if (notesData.length === 0) {
+          return "No notes found.";
+      }
+
       const data = notesData.map((data) => {
         return  `ID: ${data.id}\nTitle: ${data.title}\nCreated: ${data.createdAt}`
       });
