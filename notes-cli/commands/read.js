18396e9fcc06f4a61d8de692cfe248db11ee0499
Bola Nabil <bolanabil404@gmail.com>
2026-06-28 13:55:34 +0300

    handling if note target found

--- notes-cli/commands/read.js
+++ notes-cli/commands/read.js
@@ -3,6 +3,10 @@ import {notesData} from "../utils/fileHelper.js";
 export const read = () => {
     const target = process.argv[3];
 
+    if (!target) {
+        return "Please provide a note title.";
+    }
+
     const foundSearch = notesData.find((data) => data.title === target);
 
     if(foundSearch) {
