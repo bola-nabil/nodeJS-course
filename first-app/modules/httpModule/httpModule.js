a54cee638e2506a7d395c1565443de6cb6bd8b07
Bola Nabil <bolanabil404@gmail.com>
2026-09-01 15:52:05 +0300

    WriteHead, write, server error and server close

--- first-app/modules/httpModule/httpModule.js
+++ first-app/modules/httpModule/httpModule.js
@@ -1,17 +1,12 @@
 import http from "http"; // http module
 
 const server = http.createServer((req, res) => {
-    console.log("request header", req.headers);
-    console.log("request method", req.method);
-    console.log("request url", req.url);
-
-    if(req.method === "GET" && req.url === "/") {
-        res.statusCode = 200;
-        res.end("Home page");
-        return;
-    }
-    res.statusCode = 404;
-    res.end("Page not found");
+    res.writeHead(200, {
+        "content-type": "text/html"
+    });
+    res.write("<h1>NovaCart Project</h1>");
+    res.write("<h1>Hello welcome</h1>");
+    res.end();
 });
 
 const PORT = 3000;
@@ -19,3 +14,11 @@ const PORT = 3000;
 server.listen(PORT, "localhost", () => {
     console.log(`Server running in port ${PORT} http://localhost:${PORT}`);
 });
+
+server.on("error", (error) => {
+    console.error("Server Error", error.message);
+});
+
+server.close(() => {
+    console.log("Server closed");
+})
