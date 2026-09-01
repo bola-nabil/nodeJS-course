b6dfb4a653b0a5f74945b2b2f77860d5c7bfb8f5
Bola Nabil <bolanabil404@gmail.com>
2026-09-01 12:11:58 +0300

    Print requests

--- first-app/modules/httpModule/httpModule.js
+++ first-app/modules/httpModule/httpModule.js
@@ -1,23 +1,21 @@
 import http from "http"; // http module
 
 const server = http.createServer((req, res) => {
+    console.log("request header", req.headers);
+    console.log("request method", req.method);
+    console.log("request url", req.url);
+
     if(req.method === "GET" && req.url === "/") {
         res.statusCode = 200;
-        res.end("Home");
-        return;
-    }
-
-    if(req.method === "GET" && req.url === "/users") {
-        res.statusCode = 200;
-        res.end("Users");
+        res.end("Home page");
         return;
     }
     res.statusCode = 404;
-    res.end("Not found");
+    res.end("Page not found");
 });
 
 const PORT = 3000;
 
 server.listen(PORT, "localhost", () => {
-    console.log(`Server running in port ${PORT} http://localhost/${PORT}`);
+    console.log(`Server running in port ${PORT} http://localhost:${PORT}`);
 });
