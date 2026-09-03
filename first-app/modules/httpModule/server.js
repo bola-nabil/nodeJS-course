873b0dc1b00e3492d2a12e2a1ba4feccb14edd68
Bola Nabil <bolanabil404@gmail.com>
2026-09-03 12:54:17 +0300

    Update server

--- first-app/modules/httpModule/server.js
+++ first-app/modules/httpModule/server.js
@@ -1,44 +1,14 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html");
-
-    console.log("Request header", req.headers);
-    
     if(req.method === "GET" && req.url === "/") {
-        res.statusCode = 200;
-         res.end(`
-            <h1>Home Page</h1>
+        res.setHeader("Content-Type", "application/json");
 
-            <nav>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
-            </nav>
-        `);
-        return;
+        res.end(JSON.stringify({
+            "name": "bola",
+            "age": 25
+        }));
     }
-
-    if(req.method === "GET" && req.url === "/about") {
-        res.statusCode = 200;
-        res.end(`
-            <h1>About Page</h1>
-            <a href="/">Home</a>
-        `);
-        return;
-    }
-
-    if(req.method === "GET" && req.url === "/contact") {
-        res.statusCode = 200;
-        res.end(`
-            <h1>Contact Page</h1>
-            <a href="/">Home</a>
-        `);
-        return;
-    }
-
-    res.statusCode = 404;
-    res.end("<h1>Page not found</h1>");
 });
 
 const PORT = 5000;
